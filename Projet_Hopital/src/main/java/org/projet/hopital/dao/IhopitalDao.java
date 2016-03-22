9546bc496aa59de778655226a04d05e093e12900
Supermus <el-madi@live.fr>
2016-03-23 00:38:12 +0100

    maintenant le projet est prés pour tester la partie metier mais il faut la terminé d'abord :)

---
+++ Projet_Hopital/src/main/java/org/projet/hopital/dao/IhopitalDao.java
@@ -0,0 +1,17 @@
+package org.projet.hopital.dao;
+
+
+import java.util.List;
+
+import org.projet.hopital.entities.*;
+
+
+public interface IhopitalDao {
+	
+	/*//public Long addPatient(Patient p);//on peut mettre void mais pour recuperer le client on doit mettre Client
+	public Long addDossier(Patient p);
+	public List<Dossier> listDossier();
+	public List<Dossier> dossierParMotCle(String cle);
+	
+	*/
+}	
--- Projet_Hopital/src/main/java/org/projet/hopital/dao/LoginDAO.java
+++
@@ -1,7 +0,0 @@
-package org.projet.hopital.dao;
-
-
-public interface LoginDAO {
-	public boolean checkLogin(String login, String mdp);
-	
-}
