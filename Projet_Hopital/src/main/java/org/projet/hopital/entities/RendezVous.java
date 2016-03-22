c55c88363c88dd4249ea0fb6f79c8c886d7e6453
bouzianeb <bou-1993@hotmail.fr>
2016-03-22 16:24:27 +0100

    bla bla...

---
+++ Projet_Hopital/src/main/java/org/projet/hopital/entities/RendezVous.java
@@ -0,0 +1,30 @@
+package org.projet.hopital.entities;
+
+public class RendezVous {
+	private String date;
+	private String heure;
+	
+	
+	public RendezVous(){
+		super();
+		
+	}
+	
+	public void RechercherRdv(){
+		
+	}
+	
+	public void AjouterRdv(){
+		
+	}
+	
+	public int ModifierRdv( ){
+		
+		return 0;
+	}
+	
+	public void SupprimerRdv(){
+		
+	}
+
+}
